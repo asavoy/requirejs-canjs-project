c5bc06366d25661360dcf056f6cf2a0783f38346
Alvin Savoy <asavoy@users.noreply.github.com>
2014-12-17 01:30:40 +1100

    pages: Add main modules, which simply load all the components.

---
+++ static/jsmvc/pages/example/example.js
@@ -0,0 +1,22 @@
+define([
+    'jquery',
+    'can',
+    'components/absolute_paths/absolute_paths',
+    'components/can_component/can_component',
+    'components/can_view/can_view',
+    'components/can_view_attr/can_view_attr',
+    'components/css_module/css_module',
+    'components/ejs_template/ejs_template',
+    'components/jquery_works/jquery_works',
+    'components/global_refs/global_refs',
+    'components/module_refs/module_refs',
+    'components/mustache_helper/mustache_helper',
+    'components/mustache_partials/mustache_partials',
+    'components/mustache_template/mustache_template',
+    'components/progressive_load/progressive_load',
+    'components/relative_paths/relative_paths',
+    'components/stache_template/stache_template',
+    'components/steal_remove_blocks/steal_remove_blocks'
+], function($) {
+
+});
