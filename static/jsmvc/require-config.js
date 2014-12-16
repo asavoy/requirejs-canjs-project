7fb36d9d29598420a21c2bb3550cca6f4c53a226
Alvin Savoy <asavoy@users.noreply.github.com>
2014-12-17 01:50:45 +1100

    require-config.js: Install requirejs-canjs-templates for loading/building CanJS templates.

--- static/jsmvc/require-config.js
+++ static/jsmvc/require-config.js
@@ -6,12 +6,25 @@ requirejs.config({
         }
     },
     paths: {
+        // RequireJS loader plugins.
+        ejs: 'requirejs-plugins/requirejs-canjs-templates/ejs',
+        mustache: 'requirejs-plugins/requirejs-canjs-templates/mustache',
+        stache: 'requirejs-plugins/requirejs-canjs-templates/stache',
+        text: 'requirejs-plugins/text/text'
     },
     shim: {
         // Shim for `jquery-migrate` to load after jQuery.
         'jquery/jquery-migrate': {
             deps: ['jquery/jquery'],
             exports: 'jQuery'
+        },
+        // Ensures that `can/view/ejs` is included in the build.
+        ejs: {
+            deps: ['can/view/ejs']
+        },
+        // Ensures that `can/view/stache` is included in the build.
+        stache: {
+            deps: ['can/view/stache']
         }
     }
 });
